e017b6622b61f6760a58cd2728fefaeded1f6e87
av993 <ameyav993@gmail.com>
2022-03-15 19:04:42 -0400

    Added database connection and create table code

--- profile.py
+++ profile.py
@@ -1,4 +1,5 @@
 
+import psycopg2
 
 
 #Check with CAS authentication and see whether user
@@ -16,8 +17,24 @@ def get_profilestatus(netid):
 
 #Create profile for user and update MongoDB
 def create_profile(netid):
+    create_user_table()
     print("Profile created for: " + netid)
     return netid
+    
+def create_user_table():
+    conn = psycopg2.connect(database="d4p66i6pnk5690", user = "uvqmavpcfqtovz", password = "e7843c562a8599da9fecff85cd975b8219280577dd6bf1a0a235fe35245973d2", host = "ec2-44-194-167-63.compute-1.amazonaws.com
+    ", port = "5432")
+
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE USER
+            (NETID TEXT PRIMARY KEY     NOT NULL,
+            NAME           TEXT    NOT NULL,
+            YEAR            INT     NOT NULL,
+            MAJOR        TEXT NOT NULL,
+            PHONENUM         TEXT NOT NULL,
+            BIO        TEXT);''')
+    conn.commit()
+    conn.close()
     
     
 
