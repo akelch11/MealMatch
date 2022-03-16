ad9af73a9033f7f51e7c9d7229dde8d13413015c
av993 <ameyav993@gmail.com>
2022-03-15 20:29:05 -0400

    Removed insert column

--- profile.py
+++ profile.py
@@ -17,12 +17,6 @@ def get_profilestatus(netid):
 
 #Create profile for user and update MongoDB
 def create_profile(netid, name, year, major, phonenum, bio):
-    conn = psycopg2.connect(database="d4p66i6pnk5690", user = "uvqmavpcfqtovz", password = "e7843c562a8599da9fecff85cd975b8219280577dd6bf1a0a235fe35245973d2", host = "ec2-44-194-167-63.compute-1.amazonaws.com", port = "5432")
-    cur = conn.cursor()
-
-    cur.execute("INSERT INTO users (NETID,NAME,YEAR,MAJOR,PHONENUM, BIO) \
-          VALUES ({}, {}, {}, {}, {})".format(netid, name, year, major, phonenum, bio));
-
     print("Profile created for: " + netid)
     return netid
     
