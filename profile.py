a92c1007615cca18e3838dcec3f9f822d17d20be
av993 <ameyav993@gmail.com>
2022-03-16 00:52:01 -0400

    Update with insert into table working

--- profile.py
+++ profile.py
@@ -19,9 +19,11 @@ def get_profilestatus(netid):
 def create_profile(netid, name, year, major, phonenum, bio):
     conn = psycopg2.connect(database="d4p66i6pnk5690", user = "uvqmavpcfqtovz", password = "e7843c562a8599da9fecff85cd975b8219280577dd6bf1a0a235fe35245973d2", host = "ec2-44-194-167-63.compute-1.amazonaws.com", port = "5432")
     cur = conn.cursor()
+    
 
-    cur.execute("INSERT INTO users (NETID,NAME,YEAR,MAJOR,PHONENUM, BIO) \
-          VALUES ({}, {}, {}, {}, {}, {})".format(netid, name, year, major, phonenum, bio));
+    sql = "INSERT INTO users (NETID,NAME,YEAR,MAJOR,PHONENUM, BIO) VALUES (%s, %s, %s, %s, %s, %s)"
+    val = (netid, name, year, major, phonenum, bio)
+    cur.execute(sql, val)
 
     print("Profile created for: " + netid)
     return netid
