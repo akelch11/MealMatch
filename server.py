d9c989a197310bb00379e64fd1673e92aa0c70a2
av993 <ameyav993@gmail.com>
2022-03-22 04:13:25 -0400

    Updated Heroku SQL statements

--- server.py
+++ server.py
@@ -57,5 +57,5 @@ def create_profile_main():
 def status():
     return jsonify({"message": "ok"})
 
-port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 5001))
 app.run(host='0.0.0.0', port=port, debug=False )
