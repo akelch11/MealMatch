d0d7e480d2eb83a34fa802ca294bae5664f7b3da
av993 <ameyav993@gmail.com>
2022-03-29 01:32:51 -0400

    update

--- matcher.py
+++ matcher.py
@@ -1,35 +1,46 @@
+from tracemalloc import start
+from venv import create
 import psycopg2
+import random
+import string
 dhall_list = ["WUCOX", "ROMA", "FORBES", "CJL", "WHITMAN"]
 
-def create_profile(mealtime, dhall, endtime):
+def add_request(netid, meal_type, start_time, end_time, dhall_arr):
     conn = psycopg2.connect(database="d4p66i6pnk5690", user = "uvqmavpcfqtovz", password = "e7843c562a8599da9fecff85cd975b8219280577dd6bf1a0a235fe35245973d2", host = "ec2-44-194-167-63.compute-1.amazonaws.com", port = "5432")
     cur = conn.cursor()
-    
 
-    sql = "INSERT INTO users (NETID,BEGINTIME,ENDTIME,MATCHID,"
+    sql = "INSERT INTO requests (REQUESTID, NETID,BEGINTIME,ENDTIME, LUNCH, MATCHID,"
     
     for i in range(len(dhall_list)):
         sql = sql + "{},".format(dhall_list[i])
 
+    sql = sql + "ATDHALL) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s, %s)"
+    print(sql)
+    requestId = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
 
-    "ATDHALL) VALUES (%s, %s, %s, %s, %s, %s)"
-    val = (netid, name, year, major, phonenum, bio)
-    cur.execute(sql, val)
+
+    val = [requestId, netid, start_time, end_time, meal_type, ""]
+    for i in range(len(dhall_arr)):
+        val.append(dhall_arr[i])
+
+    val.append(True)
+    cur.execute(sql, tuple(val))
 
     conn.commit()
     conn.close()
     print("Profile created for: " + netid)
 
-    return netid
 def create_requests_table():
     conn = psycopg2.connect(database="d4p66i6pnk5690", user = "uvqmavpcfqtovz", password = "e7843c562a8599da9fecff85cd975b8219280577dd6bf1a0a235fe35245973d2", host = "ec2-44-194-167-63.compute-1.amazonaws.com", port = "5432")
 
     cur = conn.cursor()
 
     create_table_query = '''CREATE TABLE requests
-            (NETID TEXT PRIMARY KEY NOT NULL,
+            (REQUESTID TEXT PRIMARY KEY NOT NULL,
+            NETID TEXT NOT NULL,
             BEGINTIME TIMESTAMP NOT NULL,
             ENDTIME TIMESTAMP NOT NULL,
+            LUNCH BOOLEAN NOT NULL,
             MATCHID TEXT,\n'''
 
     for i in range(len(dhall_list)):
