c36a848c2c5b168fd9d85ffd3d1bed87242f815e
Adam Kelch <askelch@gmail.com>
2022-04-28 12:28:51 -0400

    add phone numbers to past matches, fix landing dimensions

--- matcher.py
+++ matcher.py
@@ -301,6 +301,16 @@ def get_name_from_netid(netid):
     return row[0]
 
 
+def get_name_and_num_from_netid(netid):
+    cur, conn = new_connection()
+    query = """SELECT name, phonenum FROM users WHERE netid = %s"""
+    cur.execute(query, [netid])
+    row = cur.fetchone()
+    close_connection(cur, conn)
+    return row[0], row[1]
+    
+
+
 def get_past_matches(netid):
     query = """SELECT first_netid, second_netid,
             end_window, dining_hall, match_id, lunch
@@ -320,13 +330,15 @@ def get_past_matches(netid):
         other_netid = match[0] if match[1] == netid else match[1]
 
         match_info['netid'] = other_netid
-        match_info['name'] = get_name_from_netid(other_netid)
+        match_info['name'], phonenum = get_name_and_num_from_netid(other_netid)
         match_info['day'] = match[2]
         match_info['dhall'] = match[3]
         match_info['id'] = match[4]
         match_info['meal'] = 'Lunch' if match[5] else 'Dinner'
+        match_info['phonenum'] = phonenum
 
         past_matches.append(match_info)
+        
 
     return past_matches
 
